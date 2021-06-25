4b349668a268b4690b169c217b384263cec0315e
mithi <mithi@mickey>
2021-06-25 18:30:11 +0200

    first changes

--- src/main/java/MyRunner/TestRunner.java
+++ src/main/java/MyRunner/TestRunner.java
@@ -7,9 +7,9 @@ import io.cucumber.junit.CucumberOptions;
 	@RunWith(Cucumber.class)
 	@CucumberOptions(
 			features = "D:/softwares/Automation Testing/Selenium Cucumber BDD/CucumberSeleniumFramework/src/main/java/Features/login.feature", //the path of the feature files
-			glue = {"D:/softwares/Automation Testing/Selenium Cucumber BDD/CucumberSeleniumFramework/src/main/java/stepDefinitions"} //the path of the step definition files
+			glue = {"stepDefinitions"}, //the path of the step definition files
 			//format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
-			//monochrome = true, //display the console output in a proper readable format
+			monochrome = true //display the console output in a proper readable format
 			//strict = true, //it will check if any step is not defined in step definition file
 			//dryRun = false //to check the mapping is proper between feature file and step def file
 			//tags = {"~@SmokeTest" , "~@RegressionTest", "~@End2End"}			
